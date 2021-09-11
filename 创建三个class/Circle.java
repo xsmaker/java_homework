c2352fd5ed23c6c971af4e481347a673fc08d6e2
Xujinsong <xujinsonghs@iclou.com>
2021-09-11 23:40:40 +0800

    java的三次作业

---
+++ 创建三个class/Circle.java
@@ -0,0 +1,18 @@
+public class Circle {
+    private double radius;
+    public Circle(double radius){
+        this.radius = radius;
+    }
+    public double getRadius(){
+        return this.radius;
+    }
+    public void setRadius(double radius){
+        this.radius= radius;
+    }
+    public double getArea(){
+        return radius*radius*Math.PI;
+    }
+    public double getPerimeter(){
+        return 2*radius*Math.PI;
+    }
+}
